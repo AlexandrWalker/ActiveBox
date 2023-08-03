7b38b362a3cfcff9a2cdb2f67001fbfb5a926680
Alexandr <alexandr.d.walker@gmail.com>
2023-08-03 23:46:34 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,60 @@
+$(function () {
+  let header = $("#header");
+  let intro = $("#intro");
+  let introH = intro.innerHeight();
+  let scrollPos = $(window).scrollTop();
+  let nav = $("#nav");
+  let navToggle = $("#navToggle");
+
+  checkScroll(scrollPos, introH);
+
+  $(window).on("scroll load resize", function () {
+    introH = intro.innerHeight();
+    scrollPos = $(this).scrollTop();
+
+    checkScroll(scrollPos, introH);
+  });
+
+  function checkScroll(scrollPos, introH) {
+    if (scrollPos > introH) {
+      header.addClass("fixed");
+    } else {
+      header.removeClass("fixed");
+    }
+  }
+
+  /* Smooth Scroll*/
+  $("[data-scroll]").on("click", function (event) {
+    event.preventDefault();
+
+    let elementId = $(this).data("scroll");
+    let elementOffset = $(elementId).offset().top;
+
+    nav.removeClass("show");
+
+    $("html, body").animate(
+      {
+        scrollTop: elementOffset - 70,
+      },
+      700
+    );
+  });
+
+  /* NavToggle */
+  navToggle.on("click", function (event) {
+    event.preventDefault();
+    nav.toggleClass("show");
+  });
+
+  /* Reviews: https://kenwheeler.github.io/slick/ */
+  let slider = $("#reviewsSlider");
+
+  slider.slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: true,
+    arrows: false,
+    dots: true,
+  });
+});
